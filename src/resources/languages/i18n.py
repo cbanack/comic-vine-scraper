7a1b672f773f912f617a0aef5024817bfa3a2e12
cbanack <cbanack@gmail.com>
2011-01-15 18:22:26 -0700

    Issue 97:  Internationalization Architecture - began work on this feature

---
+++ src/resources/languages/i18n.py
@@ -0,0 +1,24 @@
+'''
+This module contains the DBPictureBox class, which can be used to display 
+remotely loaded images using an asyncrhonous off-loading thread manner.
+
+@author: Cory Banack
+'''
+#import clr
+#import resources
+#import utils
+
+#==============================================================================
+class I18n(object):
+   '''
+   coryhigh: implement this class/module (which should it be?)
+   '''
+
+   #===========================================================================
+   def __init__(self):
+      pass
+
+# this worked!
+#def i18n()
+#      log.debug("Localized: ", 
+#         ComicRack.Localize("Script.ComicVineScraper", "Test", "Didn't Work!"));
