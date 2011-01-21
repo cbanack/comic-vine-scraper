dd132c69639acbd0ffcd23076cb48ad24ab6aea1
cbanack <cbanack@gmail.com>
2011-01-20 22:05:51 -0700

    Issue 97:  Internationalization Architecture - completed the I18n module. - now its time to actually externalize all the scripts strings!

--- src/resources/resources.py
+++ src/resources/resources.py
@@ -13,6 +13,9 @@ _SCRIPT_DIRECTORY =  __file__[:-len('resources.py')]
 LOCAL_CACHE_DIRECTORY = _SCRIPT_DIRECTORY + 'localCache/'
 SETTINGS_FILE = _SCRIPT_DIRECTORY + 'settings.dat'
 GEOMETRY_FILE = _SCRIPT_DIRECTORY + 'geometry.dat'
+I18N_DEFAULTS_FILE = _SCRIPT_DIRECTORY + 'en.zip'
+I18N_XML_ENTRY = 'Script.ComicVineScraper.xml'
+
 
 # do NOT change the following lines.  they are modified by the build process!
 SCRIPT_VERSION = "!DEV!"
