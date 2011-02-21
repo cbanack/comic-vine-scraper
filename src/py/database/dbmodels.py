1241c2ec9009b856b5de8c198bb9f3d128c88434
cbanack <cbanack@gmail.com>
2011-02-21 15:11:15 -0700

    Issue 97:  Internationalization Architecture
    - created dberrors.py
    - commenting and cleaning up dbmodels

--- src/py/database/dbmodels.py
+++ src/py/database/dbmodels.py
@@ -1,4 +1,7 @@
 '''
+This module defines the 'model' classes for the database described in db.py.
+That is, it defines classes to hold the data retrieved by the database's query
+methods.
 
 @author: Cory Banack
 '''
@@ -6,24 +9,59 @@
 #corylow: comment and cleanup this file
 
 from utils import sstr
-class IssueRef(object): 
+
+#==============================================================================
+class IssueRef(object):
+   '''
+   This class contains minimal reference details about a single issue of a 
+   comic book in an unspecified series and database.  It is a lightweight, 
+   immutable object, suitable for storing and sorting in large collections.
+   '''
+   
+   #===========================================================================
    def __init__(self, issue_num_s, issue_key):
-      # issue_num_s can be '' or any other string
-      # issue key must be something ('not issue_key' == False)
-      if not issue_key or issue_num_s is None:
+      ''' 
+      Initializes a newly created IssueRef, checking the given parameters to
+      make sure they are legal, and then storing them as read-only properties.
+      
+      issue_num_s --> a string describing the comics issue number (which may 
+         not be a number at all, it can be '' or '1A' or 'A', etc. It cannot
+         be None.
+         
+      issue_key --> a database specific object (i.e. the 'memento' design 
+         pattern) that can be used by the database at a later date to 
+         unambiguously identify the comic book that this IssueRef represents.
+         This cannot be None, and it should have a useful __str__ method. 
+      '''
+
+      if issue_key is None or issue_num_s is None:
          raise Exception()
       
-      self.issue_key = issue_key
-      self.issue_num_s = issue_num_s.strip()
+      self.__issue_key = issue_key
+      self.__issue_num_s = issue_num_s.strip()
       
       # used only for comparisons
       self._cmpkey_s = sstr(self.issue_key)
       
+      
+   #===========================================================================
+   # the 'number' of this IssueRef's issue, as a string. not None. maybe empty.
+   issue_num_s = property( lambda self : self.__issue_num_s )
+   
+   # the db key (i.e. a memento object) of this IssueRef's issue. not None.
+   issue_key = property( lambda self : self.__issue_key )
+      
+      
+   #===========================================================================
    def __str__(self):
+      ''' Implements "to string" functionality for standard python objects. '''
       return "Issue #" + sstr(self.issue_num_s) \
          + " (" + sstr(self.issue_key) + ")"
          
+         
+   #===========================================================================
    def __cmp__(self, other):
+      ''' Implements comparisons for standard python objects. '''
       if not hasattr(other, "_cmpkey_s"):
          return -1
       
@@ -32,13 +70,29 @@ class IssueRef(object):
       else:
          return 1 if self._cmpkey_s > other._cmpkey_s else 0
       
+      
+   #===========================================================================
    def __hash__(self):
+      ''' Implements the hash function for standard python objects. '''
       return self._cmpkey_s.__hash__() 
 
+
+
+
+
       
+#==============================================================================
 class SeriesRef(object):
+   '''
+   This class contains minimal reference details about a series of comic books 
+   in an unspecified database.  It is a lightweight, immutable object, 
+   suitable for storing and sorting in large collections.
+   '''
+   
+   #===========================================================================
    def __init__(self, series_key, series_name_s, start_year_s, publisher_s,
          issue_count_s, thumb_url_s):
+      ''' coryhigh: START HERE '''
       # series name will become series key if empty
       # series key must be something ('not issue_key' == False)
       # start_year can be '', or digits
@@ -60,9 +114,11 @@ class SeriesRef(object):
       # used only for comparisons
       self._cmpkey_s = sstr(self.series_key)
       
+   #===========================================================================
    def __str__(self):
       return sstr(self.series_name_s) + " (" + sstr(self.series_key) + ")"
    
+   #===========================================================================
    def __cmp__(self, other):
       if not hasattr(other, "_cmpkey_s"):
          return -1
@@ -72,11 +128,18 @@ class SeriesRef(object):
       else:
          return 1 if self._cmpkey_s > other._cmpkey_s else 0
       
+   #===========================================================================
    def __hash__(self):
       return self._cmpkey_s.__hash__()
+
+
+
        
    
+#==============================================================================
 class Issue(object):
+   
+   #===========================================================================
    def __init__(self):
       self.issue_key = ''
       self.issue_num_s = ''
@@ -115,36 +178,12 @@ class Issue(object):
 #      del self.__title_s
 #      
 #   title_s = property(get_title_s, set_title_s, del_title_s, "bah")
-#      
+#
+      
+   #===========================================================================
    def __str__(self):
       return "Issue #" + sstr(self.issue_num_s) \
-         + " (" + sstr(self.issue_key) + ")" 
+         + " (" + sstr(self.issue_key) + ")"
+         
+   # coryhigh: hash? __cmp__? 
    
-   
-# =============================================================================
-class DatabaseConnectionError(Exception):
-   ''' 
-   A special exception that gets thrown anytime there is an network error while
-   trying to contact the scraper database.   This is normally because the 
-   database is down or unresponsive, or the user's internet connection is down.
-   '''
-   
-   # ==========================================================================
-   def __init__(self, database_name_s, url_s, underlying):
-      ''' 
-      database_name_s -> the name of the database that raised this error
-      url_s -> the url that caused the problem
-      underlying => the underlying io exception or error
-      '''
-      
-      super(Exception,self).__init__(sstr(database_name_s) +
-         " database could not be reached\n"\
-         "url: " + url_s + "\nCAUSE: " + 
-         sstr(underlying).replace('\r','') ) # .NET exception
-      self.__database_name_s = sstr(database_name_s)
-      
-   # ==========================================================================   
-   def db_name_s(self):
-      ''' Returns the name of the database that raised this exception. '''
-      
-      return self.__database_name_s
