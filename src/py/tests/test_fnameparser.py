3832965ecd1c5093d8a089ffa00a1bd46d15455f
cbanack <cbanack@gmail.com>
2011-11-06 13:29:34 -0700

    Added more filename parsing unit tests, stored in a separate data file.

--- src/py/tests/test_fnameparser.py
+++ src/py/tests/test_fnameparser.py
@@ -4,19 +4,50 @@ This module contains all unittests for the fnameparser module.
 Created on Oct 26, 2011
 @author: cbanack
 '''
+import clr
+import re
 from unittest import TestCase, TestSuite
 from fnameparser import extract
+
+clr.AddReference('System')
+from System.IO import File, StreamReader
+from System.Text import Encoding
 
 #==============================================================================
 def load_tests(loader, tests, pattern):
    ''' Returns all of the testcases in this module as a testsuite '''
-   tests = [ 
-      ("2000AD 1740 (29-06-11) (John Williams-DCP)", "2000AD", "1740"), 
-      ("2000AD 0001 (29-06-11)", "2000AD", "1"), 
-   ]
-   return TestSuite( [ TestFnameParser(x) for x in tests ] )
+   file = __file__[:-(len(__name__) + len('.py'))] + r"\test_fnameparser.data"
+   return TestSuite( [ TestFnameParser(x) for x in __load_testdata(file) ] )
 
-
+#==============================================================================
+def __load_testdata(file):
+   """
+   Reads the testdata out of a file.  Testdata consists of exactly three 
+   strings on each line, each one enclosed in quotation marks (" or ').  
+   The first is the filename to be parsed, the second is the series name
+   that should be parsed out of it, and the third is the issue number string
+   that should be parsed out of it.
+   
+   Blank lines and lines that begin with # are ignored.
+   """
+   retval = []
+   if File.Exists(file): 
+      with StreamReader(file, Encoding.UTF8, False) as sr:
+         line = sr.ReadLine()
+         while line is not None:
+            line = line.strip()
+            if len(line) > 0 and not line.startswith("#"):
+               if line.startswith('"'):
+                  data = re.findall(r'".*?"', line)
+               else:
+                  data = re.findall(r"'.*?'", line)
+               if len(data) != 3:
+                  raise Exception("badly formatted test data");
+               retval.append( [x.strip('"\'') for x in data] ) 
+            line = sr.ReadLine()
+   return retval
+     
+      
 #==============================================================================
 class TestFnameParser(TestCase):
 
--- src/py/utils/fnameparser.py
+++ src/py/utils/fnameparser.py
@@ -16,13 +16,42 @@ def extract( filename_s ):
    ''' 
    series_s = ""
    issue_num_s = ""
-   first_bracket_idx = filename_s.find('(')
-   if first_bracket_idx > 0: 
-      s = filename_s[0:first_bracket_idx]
-      match = re.match(r"^(.*?)#?\s*(-?[0-9]+[.0-9]*)\s*$", s)
-      if match:
-         series_s = match.group(1).strip()
-         issue_num_s = float(match.group(2).strip()) \
-            if '.' in match.group(2) else int(match.group(2).strip())
-         issue_num_s = utils.sstr(issue_num_s) 
+   s = filename_s
+   
+   # 1. find the last period, and remove everything after it.  it's not a 
+   #    valid comicbook file if it doesn't have an extension.
+   last_period = s.rfind(r".")
+   if last_period > 0:
+      s = s[0:last_period]
+      
+   # 2. strip out all bracketed data from the name, as well as all volume info
+   #    gotta handle things like "vol. 2a" and "vol -3.1"
+   def recurse_sub(pattern, s):
+      while re.search(pattern, s):
+         s = re.sub(pattern, "", s)
+      return s
+   s = recurse_sub(r"\([^\(]*?\)", s)
+   s = recurse_sub(r"\{[^\{]*?\}", s)
+   s = recurse_sub(r"\[[^\[]*?\]", s)
+   s = re.sub(r"(?i)((v|vol)\.?|volume)\s*-?\s*[0-9]+[.0-9a-z]*", "", s)
+   
+   
+   # 3. clean up whitespace
+   s = re.sub(r"_", " ", s) 
+   s = re.sub(r"\s{2,}", " ", s) 
+   s = s.strip()
+   
+   # coryhigh: start here, count the number of "numbers", pick and remove
+   match = re.match(r"^(.*?)#?\s*(-?\s*[0-9]+[.0-9a-z]*)\s*$", s)
+   if match:
+      series_s = match.group(1).strip()
+      issue_num_s = match.group(2).strip().replace(" ", "")
+      if re.match("^[-.0-9]+$", issue_num_s):
+         issue_num_s = float(issue_num_s) \
+            if '.' in issue_num_s else int(issue_num_s)
+      issue_num_s = utils.sstr(issue_num_s) 
+   else:
+      series_s = s
+      issue_num_s = ""
+      
    return [series_s, issue_num_s]
