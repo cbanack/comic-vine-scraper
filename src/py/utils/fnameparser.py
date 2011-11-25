5c2430d7d7b54dbf81e328444178f949d6f615c7
cbanack <cbanack@gmail.com>
2011-11-24 22:21:58 -0700

    Added reading-list-style name format to the filename parser,  and associated unit tests

--- src/py/utils/fnameparser.py
+++ src/py/utils/fnameparser.py
@@ -66,14 +66,17 @@ def __extract(name_s):
    # 6. get an ordered list of issue number-like strings in the filename
    #    for example:  3, #4, 5a, 6.00, 10.0b, .5, -1.0   
    #    also, remove numbers that look like years, EXCEPT on the "2000AD" series
-   matches = list(re.finditer(r"(?u)(^|[\s#])(-?\d*\.?\d\w*)", s))
-   is2000AD = re.match(r"(?i)\s*2000[\s\.-_]*a[\s.-_]*d.*", s) 
-   if not is2000AD:
-      def isYear(d): 
-         return re.match(r"^\d{4}$",d) and int(d) > 1950 and int(d) < 2100 
-      matches = [x for x in matches if not isYear(x.group(2))]  
+   matches = __extract_numbers(s)
       
-   # 7. if we parsed out some potential issue numbers, designate the LAST 
+   # 7. if there's multiple numbers in the filename, and it starts with 
+   #    something like "05. " or "12 - " we assuming these files are part of 
+   #    a reading list, and we strip out that first part.
+   pattern = r"^\s*\d+(\.\s+|\s*-\s*(?=\D))"
+   if len(matches) > 1 and re.match(pattern,s):
+      s = re.sub(pattern, "", s, 1)
+      matches = __extract_numbers(s)
+      
+   # 8. if we parsed out some potential issue numbers, designate the LAST 
    #    (rightmost) one s the actual issue number, and remove it from the name
    if len(matches) > 0: 
       issue_num_s = matches[-1].group(2)
@@ -86,9 +89,24 @@ def __extract(name_s):
       issue_num_s = ""
       series_s = s
 
-   # 8. contract repeating whitespace, and strip bad chars off the ends      
+   # 9. contract repeating whitespace, and strip bad chars off the ends      
    series_s = re.sub(r"\s{2,}", " ", series_s).strip(" ,-_").strip() 
       
    return [series_s, issue_num_s]
 
 
+#==============================================================================
+def __extract_numbers(s):
+   '''  
+   Searches through the given string left-to-right, building an ordered list of
+   "issue number-like" substrings.  For example, this method finds substrings 
+   like:  3, #4, 5a, 6.00, 10.0b, .5, -1.0
+   '''   
+   matches = list(re.finditer(r"(?u)(^|[\s#])(-?\d*\.?\d\w*)", s))
+   # remove matches that look like years, EXCEPT on the "2000AD" series
+   is2000AD = re.match(r"(?i)\s*2000[\s\.-_]*a[\s.-_]*d.*", s) 
+   if not is2000AD:
+      def isYear(d): 
+         return re.match(r"^\d{4}$",d) and int(d) > 1950 and int(d) < 2100 
+      matches = [x for x in matches if not isYear(x.group(2))]
+   return matches  
