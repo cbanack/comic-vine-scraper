36ba2219e9c4bfab2e9f17ee47e188a5024372da
cbanack <cbanack@gmail.com>
2013-03-04 22:03:06 -0700

    Issue 274:  Autoscrape useing Image Comparisons - cleaned up Main Processing Loop in scrape engine to accomodate auto scraping.

--- src/py/utils/automatcher.py
+++ src/py/utils/automatcher.py
@@ -5,11 +5,11 @@ automatically find a good match for a given ComicBook object.
 @author: Cory Banack
 '''
 import clr
-from dbmodels import IssueRef
+from dbmodels import IssueRef, SeriesRef
 clr.AddReference('System')
 
 #==============================================================================
-def find_image_ref(book):
+def find_series_ref(book):
    ''' 
    Performs a number of queries on the database, and attempts to find an 
    IssueRef object that describes the given book.  Returns None if no 
