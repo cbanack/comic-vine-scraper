4bb3421dc50986da31e0a544efdf892783ff2244
cbanack <cbanack@gmail.com>
2013-04-26 22:06:30 -0600

    Issue 274: Autoscrape by Using Image Comparisons - got a robust image comparison algorithm working

--- src/py/utils/imagehash.py
+++ src/py/utils/imagehash.py
@@ -5,30 +5,54 @@ to see if they are identical or not.
 @author: Cory Banack
 '''
 import clr
+import log
 clr.AddReference('System')
 from System import Array, Single
 
 clr.AddReference('System.Drawing')
-from System.Drawing import Bitmap, Color, Graphics, GraphicsUnit, Rectangle
-from System.Drawing.Imaging import ColorMatrix, ImageAttributes
+from System.Drawing import Bitmap, Graphics, GraphicsUnit, Image, Rectangle
+from System.Drawing.Imaging import ColorMatrix, ImageAttributes, PixelFormat
 from System.Drawing.Drawing2D import CompositingQuality, \
    SmoothingMode, InterpolationMode
 
 
 #==============================================================================
-def perceptual_hash(image, ignore_lower_left=True):
+def hash(image):
    ''' 
-   Returns an image hash for the given Image. 
+   Returns an image hash for the given .NET Image.  The hash values for two 
+   images can be compared by calling:
    
-   If ignore_lower_left is True, the lower left corner of the image does not
-   affect the returned hash result.
+        similarity(hash(image1), hash2(image2)).
    
+   The given images are not modified in any way, nor are they Disposed.
    '''
+   return __perceptual_hash(image)       
+         
+         
+#==============================================================================
+def similarity(hash1, hash2):
+   ''' 
+   Returns the 'similarity' between two image hash values as
+   a value between 0.0 and 1.0, with 1.0 meaning 'very similar'
+   and 0.0 meaning 'very different'. 
+   '''
+   if hash1 == None or hash2 == None:
+      return 0.0;
+   else:
+      xor = hash1 ^ hash2
+      xor = bin(xor)[2:].zfill(64)
+      hamming_distance = sum( b == '1' for b in xor)
+      return 1.0 - ( hamming_distance / float(len(xor)) )
+   
+   
+#==============================================================================
+def __perceptual_hash(image):
+   '''  Returns a 'perceptual' image hash for the given Image. '''
    
    if image is not None:
-   
+
       SIZE = 8
-         
+      
       # create ImageAttributes for converting image to greyscale
       # see: http://tech.pro/tutorial/660/
       #              csharp-tutorial-convert-a-color-image-to-grayscale
@@ -43,53 +67,49 @@ def perceptual_hash(image, ignore_lower_left=True):
          )))
       )
       
-      with Bitmap(SIZE,SIZE, image.PixelFormat ) as small_image:
+      with Bitmap(SIZE,SIZE, PixelFormat.Format64bppArgb ) as small_image:
          with Graphics.FromImage(small_image) as g:
             
-            # draw image in greyscale in a tiny square
-            # see:  https://www.memonic.com/user/aengus/folder/coding/id/1qVeq
-            g.CompositingQuality = CompositingQuality.HighQuality
-            g.SmoothingMode = SmoothingMode.HighQuality
-            g.InterpolationMode = InterpolationMode.HighQualityBicubic
-            g.DrawImage(image, Rectangle(0,0,SIZE,SIZE), 0, 0,
-               image.Width, image.Height, GraphicsUnit.Pixel, attr)
-            
-            # blot out the lower left corner if requested 
-            # coryhigh: a better solution: in "similarity", allow corners or 
-            # bands to be ignored....?
-            if ignore_lower_left:
-               small_image.SetPixel(0,5,Color.Black)
-               small_image.SetPixel(0,6,Color.Black)
-               small_image.SetPixel(0,7,Color.Black)
-               small_image.SetPixel(1,5,Color.Black)
-               small_image.SetPixel(1,6,Color.Black)
-               small_image.SetPixel(1,7,Color.Black)
-               small_image.SetPixel(2,6,Color.Black)
-               small_image.SetPixel(2,7,Color.Black)
-            
-            # convert image pixels into bits, where 1 means pixel is greater
-            # than image average, and 0 means pixel is less than average.
-            # return bits as a single long value
-            pixels = [small_image.GetPixel(x,y).R 
-                      for x in range(SIZE) for y in range(SIZE)];
-            average = reduce(lambda x,y: x+y, pixels) / float(len(pixels))
-            bits = map(lambda x: 1 if x > average else 0, pixels ) 
-            return reduce(lambda x, (i, val): x | (val<<i), enumerate(bits), 0)
+               # draw image in greyscale in a tiny square
+               # see: https://www.memonic.com/user/aengus/folder/coding/id/1qVeq
+               g.CompositingQuality = CompositingQuality.HighQuality
+               g.SmoothingMode = SmoothingMode.HighQuality
+               g.InterpolationMode = InterpolationMode.HighQualityBicubic
+               g.DrawImage(image, Rectangle(0,0,SIZE,SIZE), 0, 0,
+                  image.Width, image.Height, GraphicsUnit.Pixel, attr)
+               
+               # convert image pixels into bits, where 1 means pixel is greater
+               # than image average, and 0 means pixel is less than average.
+               # return bits as a single long value
+               pixels = [small_image.GetPixel(x,y).R 
+                         for x in range(SIZE) for y in range(SIZE)];
+               average = reduce(lambda x,y: x+y, pixels) / float(len(pixels))
+               bits = map(lambda x: 1 if x > average else 0, pixels ) 
+               return reduce(lambda x, (i, val): x|(val<<i), enumerate(bits), 0)
          
    else:
       return long(0);
-         
-         
+   
+
 #==============================================================================
-def similarity(hash1, hash2):
-   ''' 
-   Returns the 'similarity' between two perceptual hash values as
-   a value between 0.0 and 1.0, with 1.0 meaning 'very similar'
-   and 0.0 meaning 'very different'. 
-   '''
-   if hash1 == None or hash2 == None:
-      return 0.0;
-   else:
-      xor = hash1 ^ hash2
-      hamming_distance = sum( b == '1' for b in bin(xor)[2:])
-      return 1.0 - ( hamming_distance / float(len(bin(xor))) )
+# this is just testing code for working on the matching algorithm
+if __name__ == '__main__':  
+   log.install()
+   
+   samples_dir = r"K:/imgcmp/"
+   bases = ["animalman", "armydark", "detective", "hip", 
+            "locke", "snow", "tower", "joe", "bedlam"]
+   compares = [(x+"-a.jpg",x+"-b.jpg") for x in bases] +\
+      [("random.jpg",x+"-a.jpg")for x in bases] 
+   
+   for file1, file2 in compares:
+      with Image.FromFile(samples_dir+file1) as image1:
+         with Image.FromFile(samples_dir+file2) as image2:
+            hash1 = hash(image1)
+            hash2 = hash(image2)
+            #log.debug()
+            #log.debug("hash1: ", bin(hash1)[2:].zfill(64))
+            #log.debug("hash2: ", bin(hash2)[2:].zfill(64))
+            score = similarity( hash1, hash2 );
+            log.debug("{0:40}: {1}% similar".format(file1+"<->"+file2, score))
+            
