3e3a583cefcb21d689953381b9deb4b67a9eab10
cbanack <cbanack@gmail.com>
2014-06-09 23:43:43 -0600

    Added a bit of throttling in to play nicer with ComicVine database.

--- src/py/database/comicvine/cvdb.py
+++ src/py/database/comicvine/cvdb.py
@@ -16,6 +16,8 @@ import utils
 from utils import is_string, sstr 
 from dbmodels import IssueRef, SeriesRef, Issue
 import cvimprints
+from System import DateTime
+from System.Threading import Thread, ThreadStart
 
 clr.AddReference('System')
 from System.Net import WebRequest
@@ -32,6 +34,13 @@ __series_details_cache = None
 # this is the comicvine api key to use when accessing the comicvine api
 # it must be set when calling initialize.
 __api_key = ""
+
+# this value is used to throttle our query speeds
+__next_query_time_ms = 0
+
+# the amount of time to wait between queries
+__QUERY_DELAY_MS = 2000 
+
 
 # =============================================================================
 def _initialize(**kwargs):
@@ -131,6 +140,8 @@ def _check_magic_file(path_s):
 # =============================================================================
 def _query_series_refs(search_terms_s, callback_function):
    ''' ComicVine implementation of the identically named method in the db.py '''
+   
+   __wait_until_ready() # throttle to keep us from going too fast
       
    series_refs = set()
    
@@ -296,6 +307,8 @@ def __cleanup_search_terms(search_terms_s, alt_b):
 def _query_issue_refs(series_ref, callback_function=lambda x : False):
    ''' ComicVine implementation of the identically named method in the db.py '''
    
+   __wait_until_ready() # throttle to keep us from going too fast
+   
    # a comicvine series key can be interpreted as an integer
    series_id_n = int(series_ref.series_key)
    cancelled_b = [False]
@@ -444,6 +457,8 @@ def _query_image( ref, lasttry = False ):
 # =============================================================================
 def _query_issue(issue_ref, slow_data):
    ''' ComicVine implementation of the identically named method in the db.py '''
+   
+   __wait_until_ready() # throttle to keep us from going too fast
    
    # interesting: can we implement a cache here?  could speed things up...
    issue = Issue(issue_ref)
@@ -729,4 +744,25 @@ def __as_list(dom):
    only element in a list if it is not.  Return [] if dom is None.
    '''  
    return dom if isinstance(dom, list) else [] if dom is None else [dom]
+
+
+# =============================================================================
+def __wait_until_ready():
+   '''
+   Waits until a fixed amount of time has passed since this function was 
+   last called.  Returns immediately if that much time has already passed.
+   '''
+    
+   global __next_query_time_ms, __QUERY_DELAY_MS 
+   time_ms = (DateTime.Now-DateTime(1970,1,1)).TotalMilliseconds
+   wait_ms = __next_query_time_ms - time_ms
+   if ( wait_ms > 0 ):
+      log.debug("waiting for: ",wait_ms)
+      t = Thread(ThreadStart(lambda x=0: Thread.CurrentThread.Sleep(wait_ms)))
+      t.Start()
+      t.Join()
+   time_ms = (DateTime.Now-DateTime(1970,1,1)).TotalMilliseconds
+   __next_query_time_ms = time_ms + __QUERY_DELAY_MS
+   
+                       
 
